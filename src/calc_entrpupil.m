5f91ec505b57c3f494568cab9b1372f2a9cc419d
Grace Kuo <gracekuo01@gmail.com>
2016-01-04 16:42:24 -0500

    doublet merit function landscape

--- src/calc_entrpupil.m
+++ src/calc_entrpupil.m
@@ -5,10 +5,14 @@ function [ r, d ] = calc_entrpupil( camera )
 %       d - distance from object plane to entrance pupil
 %
 % Calculated by imaging each surface through all prior surfaces. The
-% smallest one is the entrane pupil
+% smallest one is the entrance pupil
 
+%%%% TO DO %%%%%%%%
+% for now, specify the entrance pupil as the first element because it is
+% computationally efficient
 
-
+r = camera(2).sd;
+d = camera(1).d;
 
 end
 
